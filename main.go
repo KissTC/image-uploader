cf967bbf9e4d173a617f1eaef41305a9040f48e7
Julio Paredes <ing.julio.code@gmail.com>
2023-12-05 18:12:48 -0600

    main md

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,28 +23,39 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, `<h1>FAQ Page</h1>
 	<ul>
-	  <li>
-		<b>Is there a free version?</b>
-		Yes! We offer a free trial for 30 days on any paid plans.
-	  </li>
-	  <li>
-		<b>What are your support hours?</b>
-		We have support staff answering emails 24/7, though response
-		times may be a bit slower on weekends.
-	  </li>
-	  <li>
-		<b>How do I contact support?</b>
-		Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
-	  </li>
+	<li>
+	<b>Is there a free version?</b>
+	Yes! We offer a free trial for 30 days on any paid plans.
+	</li>
+	<li>
+	<b>What are your support hours?</b>
+	We have support staff answering emails 24/7, though response
+	times may be a bit slower on weekends.
+	</li>
+	<li>
+	<b>How do I contact support?</b>
+	Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+	</li>
 	</ul>
 	`)
 }
 
+func getGalleryHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	id := chi.URLParam(r, "id")
+	fmt.Fprint(w, "<h2>galerias de "+id+"</h2>")
+}
+
 func main() {
 	r := chi.NewRouter()
+
+	// middlewares
+	r.Use(middleware.Logger)
+
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.Get("/galleries/{id}", getGalleryHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "page not found", http.StatusNotFound)
 	})
