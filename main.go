1df4ac5336985988f66e490a8bc3e76e0857815b
Julio Paredes <ing.julio.code@gmail.com>
2023-12-10 09:03:36 -0600

    adding the embed html to binary

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/kisstc/image_uploader/controllers"
+	"github.com/kisstc/image_uploader/templates"
 	"github.com/kisstc/image_uploader/views"
 )
 
@@ -17,14 +17,14 @@ func main() {
 	r.Use(middleware.Logger)
 
 	// parse the templates before starting html
-	tpl := views.Must(views.Parse(filepath.Join("templates", "home.html")))
+	tpl := views.Must(views.ParseFS(templates.FS, "home.html"))
 	r.Get("/", controllers.StaticHandler(tpl))
 
 	//r.Get("/", homeHandler)
-	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.html")))
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.html"))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.html")))
+	tpl = views.Must(views.ParseFS(templates.FS, "faq.html"))
 	r.Get("/faq", controllers.StaticHandler(tpl))
 
 	// r.Get("/galleries/{id}", getGalleryHandler)
