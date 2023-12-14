e18a810e6e3eeb69e09283cb2806751897c215bc
Julio Paredes <ing.julio.code@gmail.com>
2023-12-14 17:47:28 -0600

    signin page and cookie

--- controllers/users.go
+++ controllers/users.go
@@ -3,12 +3,16 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/kisstc/image_uploader/models"
 )
 
 type Users struct {
 	Templates struct {
-		New Template
+		New    Template
+		SignIn Template
 	}
+	UserService *models.UserService
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +26,50 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 
 // parsing the signup form, --> POST - respond when user submit the form
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "email: ", r.FormValue("email"))
-	fmt.Fprint(w, "password: ", r.FormValue("password"))
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "user created: %+v", user)
+
+}
+
+func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	// view to render
+	u.Templates.SignIn.Execute(w, data)
+}
+
+func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email    string
+		Password string
+	}
+	data.Email = r.FormValue("email")
+	data.Password = r.FormValue("password")
+
+	user, err := u.UserService.Authenticate(data.Email, data.Password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "invalid login", http.StatusInternalServerError)
+		return
+	}
+
+	cookie := http.Cookie{
+		Name:  "email",
+		Value: user.Email,
+		Path:  "/",
+	}
+	http.SetCookie(w, &cookie)
+
+	fmt.Fprintf(w, "user authenticated: %+v", user)
 }
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/kisstc/image_uploader/controllers"
+	"github.com/kisstc/image_uploader/models"
 	"github.com/kisstc/image_uploader/templates"
 	"github.com/kisstc/image_uploader/views"
 )
@@ -31,10 +32,26 @@ func main() {
 	tpl := views.Must(views.ParseFS(templates.FS, "faq.html", "tailwind.html"))
 	r.Get("/faq", controllers.FAQ(tpl))
 
-	usersC := controllers.Users{}
+	cfg := models.DefaultPostgresConfig()
+	db, err := models.Open(cfg)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	userService := models.UserService{
+		DB: db,
+	}
+
+	usersC := controllers.Users{
+		UserService: &userService,
+	}
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.html", "tailwind.html"))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.html", "tailwind.html"))
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
+	r.Post("/signin", usersC.ProcessSignIn)
 
 	// r.Get("/galleries/{id}", getGalleryHandler)
 	// r.NotFound(func(w http.ResponseWriter, r *http.Request) {
