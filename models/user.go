72634aaad0fa0501d33429f319b407f7e78f995a
Julio Paredes <ing.julio.code@gmail.com>
2024-01-19 16:57:44 -0600

    handling errors better - email address

--- models/user.go
+++ models/user.go
@@ -2,10 +2,17 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"strings"
 
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgerrcode"
 	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrEmailTaken = errors.New("models: email address is already in use")
 )
 
 // the same as the users table but in go xdxdxd
@@ -38,9 +45,14 @@ func (us *UserService) Create(email, password string) (*User, error) {
 	row := us.DB.QueryRow(`
 	INSERT INTO users(email, password_hash) VALUES ($1, $2) RETURNING id;
 	`, email, passwordHash)
-
 	err = row.Scan(&user.ID)
 	if err != nil {
+		var pgError *pgconn.PgError
+		if errors.As(err, &pgError) {
+			if pgError.Code == pgerrcode.UniqueViolation {
+				return nil, ErrEmailTaken
+			}
+		}
 		return nil, fmt.Errorf("create user: %w", err)
 	}
 
